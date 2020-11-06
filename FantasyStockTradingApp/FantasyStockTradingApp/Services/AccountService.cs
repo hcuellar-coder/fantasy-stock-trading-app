174ed016a048dbd85f29d042f6eabab1baf5f121
Eddie Cuellar <heriberto.cuellar1329@gmail.com>
2020-11-06 00:07:05 -0600

    Added Holding DB CRUD services

--- FantasyStockTradingApp/FantasyStockTradingApp/Services/AccountService.cs
+++ FantasyStockTradingApp/FantasyStockTradingApp/Services/AccountService.cs
@@ -3,21 +3,82 @@ using NHibernate;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FantasyStockTradingApp.Models;
+using NHibernate.Linq;
+using System.Security.AccessControl;
+using NHibernate.SqlCommand;
+using NHibernate.Impl;
 
 namespace FantasyStockTradingApp.Services
 {
     public interface IAccountService
     {
-
+        Task NewAccount(int user_id, float balance, float portfolio_balance);
+        Task UpdateBalance(int account_id, float balance, float portfolio_balance);
     }
 
     public class AccountService : IAccountService
     {
-        private readonly ISession _sesison;
+        private readonly ISession _session;
 
         public AccountService(ISession session)
         {
-            _sesison = session;
+            _session = session;
+        }
+
+        public async Task NewAccount(int user_id, float balance, float portfolio_balance)
+        {
+            Console.WriteLine("account_id = " + user_id);
+            Console.WriteLine("balance = " + balance);
+            Console.WriteLine("portfolio_balance = " + portfolio_balance);
+
+            try
+            {
+                using (ITransaction transaction = _session.BeginTransaction())
+                {
+                    var account = new Account
+                    {
+                        User_Id = user_id,
+                        Balance = 100000,
+                        Portfolio_Balance = 0
+                    };
+
+                    await _session.SaveAsync(account);
+                    await transaction.CommitAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                var errorString = $"Error inserting user: { ex }";
+                throw new Exception(errorString);
+            }
+
+        }
+
+        public async Task UpdateBalance(int account_id, float balance, float portfolio_balance)
+        {
+            Console.WriteLine("account_id = " + account_id);
+            Console.WriteLine("balance = " + balance);
+            Console.WriteLine("portfolio_balance = " + portfolio_balance);
+
+            try
+            {
+                using (ITransaction transaction = _session.BeginTransaction())
+                { 
+                    var query =  _session.CreateQuery("Update Account set balance =:balance, " +
+                        "portfolio_balance =:portfolio_balance where account_id =:account_id");
+                    query.SetParameter("balance", balance);
+                    query.SetParameter("portfolio_balance", portfolio_balance);
+                    query.SetParameter("account_id", account_id);
+                    await query.ExecuteUpdateAsync();
+                    await transaction.CommitAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                var errorString = $"Error inserting user: { ex }";
+                throw new Exception(errorString);
+            }
         }
 
     }
--- FantasyStockTradingApp/FantasyStockTradingApp/Services/HoldingsService.cs
+++ FantasyStockTradingApp/FantasyStockTradingApp/Services/HoldingsService.cs
@@ -10,8 +10,12 @@ namespace FantasyStockTradingApp.Services
 {
     public interface IHoldingsService
     {
+        IQueryable<Holdings> GetHoldings(int account_id);
         Task UpdateHoldings(int account_id, string symbol, int stock_count, 
                         float latest_cost_per_stock, string updated_time);
+        Task NewHolding(int account_id, string symbol, int stock_count,
+                        float latest_cost_per_stock, string last_Updated);
+        Task DeleteHolding(int account_id, string symbol);
     }
 
     public class HoldingsService : IHoldingsService
@@ -23,6 +27,24 @@ namespace FantasyStockTradingApp.Services
             _session = session;
         }
 
+        public IQueryable<Holdings> GetHoldings(int account_id)
+        {
+            try
+            {
+                using (ITransaction transaction = _session.BeginTransaction())
+                {
+                    var result = _session.Query<Holdings>()
+                        .Where(holdings => holdings.Account_Id == account_id);
+                    return result;
+                }
+            }
+            catch (Exception ex)
+            {
+                var errorString = $"User does not exist: { ex }";
+                throw new Exception(errorString);
+            }
+        }
+
         public async Task UpdateHoldings(int account_id, string symbol, int stock_count,
                         float latest_cost_per_stock, string last_Updated)
         {
@@ -30,10 +52,47 @@ namespace FantasyStockTradingApp.Services
             Console.WriteLine("symbol = " + symbol);
             Console.WriteLine("stock_count = " + stock_count);
             Console.WriteLine("latest_cost_per_stock = " + latest_cost_per_stock);
-            Console.WriteLine("updated_time = " + last_Updated);
+            Console.WriteLine("last_Updated = " + last_Updated);
             
-            
-            
+            try
+            {
+                using (ITransaction transaction = _session.BeginTransaction())
+                {
+                    var holdingExists = _session.QueryOver<Holdings>()
+                        .Where(holdings => holdings.Account_Id == account_id && holdings.Symbol == symbol)
+                        .RowCount() > 0;
+
+                    if (holdingExists)
+                    {
+                        var query = _session.CreateQuery("Update Holdings set stock_count =:stock_count, " +
+                       "latest_cost_per_stock =:latest_cost_per_stock, last_Updated :=last_Updated " +
+                       "where account_id =:account_id and symbol =:symbol");
+                        query.SetParameter("stock_count", stock_count);
+                        query.SetParameter("latest_cost_per_stock", latest_cost_per_stock);
+                        query.SetParameter("last_Updated", last_Updated);
+                        query.SetParameter("account_id", account_id);
+                        query.SetParameter("symbol", symbol);
+
+                        await query.ExecuteUpdateAsync();
+                        await transaction.CommitAsync();
+
+                    } else
+                    {
+                      await NewHolding(account_id, symbol, stock_count,
+                         latest_cost_per_stock, last_Updated);
+                    }                  
+                }
+            }
+            catch (Exception ex)
+            {
+                var errorString = $"Error inserting user: { ex }";
+                throw new Exception(errorString);
+            }
+        }
+
+        public async Task NewHolding(int account_id, string symbol, int stock_count,
+                        float latest_cost_per_stock, string last_Updated)
+        {
             try
             {
                 using (ITransaction transaction = _session.BeginTransaction())
@@ -45,8 +104,8 @@ namespace FantasyStockTradingApp.Services
                         Stock_Count = stock_count,
                         Latest_cost_per_stock = latest_cost_per_stock,
                         Last_Updated = last_Updated,
-
                     };
+
                     await _session.SaveAsync(holdings);
                     await transaction.CommitAsync();
                 }
@@ -57,6 +116,26 @@ namespace FantasyStockTradingApp.Services
                 throw new Exception(errorString);
             }
         }
+        public async Task DeleteHolding(int account_id, string symbol)
+        {
+            try
+            {
+                using (ITransaction transaction = _session.BeginTransaction())
+                {
+                    var query = _session.CreateQuery("Delete from Holdings " +
+                        "where account_id =:account_id and symbol =:symbol");
+                    query.SetParameter("account_id", account_id);
+                    query.SetParameter("symbol", symbol);
 
+                    await query.ExecuteUpdateAsync();
+                    await transaction.CommitAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                var errorString = $"Error inserting user: { ex }";
+                throw new Exception(errorString);
+            }
+        }
     }
 }
