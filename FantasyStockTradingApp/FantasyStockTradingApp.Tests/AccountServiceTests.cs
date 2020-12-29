9d970132933edd44db604609e0a0e9bb60d66fad
Eddie Cuellar <heriberto.cuellar1329@gmail.com>
2020-12-28 20:45:57 -0600

    working on New Account service test

--- FantasyStockTradingApp/FantasyStockTradingApp.Tests/AccountServiceTests.cs
+++ FantasyStockTradingApp/FantasyStockTradingApp.Tests/AccountServiceTests.cs
@@ -1,12 +1,12 @@
-using NUnit.Framework;
-using NSubstitute;
-using NHibernate;
 using FantasyStockTradingApp.Core.Entities;
 using FantasyStockTradingApp.Core.Services;
-using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using NHibernate;
+using NSubstitute;
+using NUnit.Framework;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
-using System;
 
 namespace Tests
 {
@@ -47,65 +47,28 @@ namespace Tests
             Assert.That(resultAccounts.Count, Is.EqualTo(1));
         }
 
+
         [Test]
-        public void GetAccount_ShouldThrowException()
+        public void NewAccount_ShouldCreateAccountAsync()
         {
             var UserId = 3;
-            var accounts = new List<Account>
+            var account = new List<Account>
             {
                 new Account
                 {
-                    Id = 2,
-                    UserId = 2,
+                    Id = 1,
+                    UserId = 3,
                     Balance = 10000,
                     PortfolioBalance = 0
                 }
             };
 
-            //_session.Query<Account>().Returns(accounts.AsQueryable());
-
-            //_session.Query<Account>().Returns(new Exception("user not found"));
-            //Need to fix this!
-            //var resultAccounts = _sut.GetAccount(UserId);
-            Assert.Throws<ArgumentException>(() => _sut.GetAccount(UserId));
-            //Assert.Throws<Exception>(() => _sut.GetAccount(UserId));
-
-            //Assert.Throws<InvalidOperationException>(() => _sut.GetAccount(UserId));
-
-            //Assert.AreEqual("")
-
-           //Assert.Throws(typeof(ArgumentException), delegate { throw new ArgumentException(); });
-           
-        }
-
-        [Test]
-        public void NewAccount_ShouldCreateAccount()
-        {
-            var UserId = 2;
-            var accounts = new List<Account>
-            {
-                new Account
-                {
-                    Id = 2,
-                    UserId = 2,
-                    Balance = 10000,
-                    PortfolioBalance = 0
-                }
-            };
-
-            //_session.Query<Account>().Returns(accounts.AsQueryable());
-            //_session.SaveAsync(accounts);
-
-            //await _session.CommitAsync();
-
-            //var resultAccounts = 
+            ITransaction transaction = _session.BeginTransaction();
+            transaction.CommitAsync();
+            _session.SaveAsync(account);
 
             _sut.NewAccount(UserId);
-            //_session.Query<Account>().Returns(accounts);
 
-            //var resultAccounts = _sut.GetAccount(UserId);
-
-            //Assert.That(resultAccounts, Is.EqualTo(accounts));
         }
     }
 }
