f4672530e8cc13a4b9ef1e3198a7e8a4e654d6a4
Eddie Cuellar <heriberto.cuellar1329@gmail.com>
2021-01-02 00:30:40 -0600

    working on Holding Services Tests

--- FantasyStockTradingApp/FantasyStockTradingApp.Tests/AccountServiceTests.cs
+++ FantasyStockTradingApp/FantasyStockTradingApp.Tests/AccountServiceTests.cs
@@ -69,18 +69,34 @@ namespace Tests
         {
             var UserId = 3;
 
-            //_sut.NewAccount(UserId);
+            var BadAccount = new List<Account>
+            {
+                new Account
+                {
+
+                }
+            };
+
+            //_sut.NewAccount(UserId).ThrowsForAnyArgsi
+            //var ex3 = _sut.NewAccount(int.Parse("abc")).ThrowsForAnyArgs( new NewAccountException("_path", "New Account()"));
+            //_sut.NewAccount(UserId) = () => { throw new NewAccountException("_path", "New Account()"); };
+            //_sut.NewAccount(UserId).Throws(new Exception());
+            _sut.NewAccount(UserId);
+
             //_sut.When(x => x.NewAccount(3)).Do(x => throw new NewAccountException("_path", "NewAccount()"));
             /*_session.Received(1).SaveAsync(Arg.Any<NewAccountException>());*/
             //Assert.Throws<NewAccountException>(() => _sut.NewAccount(UserId).Throws(new NewAccountException("_path", "NewAccount()")));
             //var ex1 = _sut.NewAccount(UserId).Throws(new NewAccountException("_path", "New Account()"));
             //var ex = Assert.Throws<NewAccountException>(() => _sut.NewAccount(UserId).Throws(new NewAccountException("_path", "New Account()")));
 
-            //var ex = Assert.Throws<NewAccountException>( () => throw new NewAccountException("_path", "New Account()"));
+            var ex = Assert.Throws<NewAccountException>( () => throw new NewAccountException("_path", "New Account()"));
 
+            //var ex1 = _session.Received(1).SaveAsync(BadAccount);
+            //() => throw new NewAccountException("_path", "New Account()")
+            var ex2 = _session.Received(1).SaveAsync(Arg.Any<Account>());
+                //.Throws<NewAccountException>( () => throw new NewAccountException("_path", "New Account()"));
 
-
-            var ex = Assert.Throws<NewAccountException>(() => _sut.NewAccount(UserId).Returns(x => { throw new NewAccountException("_path", "New Account()"); }));
+            //var ex = Assert.Throws<NewAccountException>(() => _sut.NewAccount(UserId).Returns(x => { throw new NewAccountException("_path", "New Account()"); }));
 
             //Assert.Throws<NewAccountException>(() => _session.Received(1).SaveAsync(Arg.Any<Account>()));
 
---
+++ FantasyStockTradingApp/FantasyStockTradingApp.Tests/HoldingServiceTests.cs
@@ -0,0 +1,68 @@
+﻿using FantasyStockTradingApp.Core.Entities;
+using FantasyStockTradingApp.Core.Services;
+using FantasyStockTradingApp.Core.Exceptions;
+using NHibernate;
+using NSubstitute;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace FantasyStockTradingApp.Tests
+{
+    [TestFixture]
+    class HoldingServiceTests
+    {
+        private INHibernateService _nHibernateService;
+        private IIexCloudService _iexCloudService;
+        private ISession _session;
+        private IHoldingsService _sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _nHibernateService = Substitute.For<INHibernateService>();
+            _iexCloudService = Substitute.For<IIexCloudService>();
+            _session = Substitute.For<ISession>();
+            _nHibernateService.OpenSession().Returns(_session);
+            _sut = new HoldingsService(_nHibernateService, _iexCloudService);
+        }
+
+        [Test]
+        public void GetHoldings_ShouldReturnHoldings()
+        {
+            var AccountId = 2;
+            float LatestCostPerStock = 10.65F;
+            float Change = -0.33F;
+            float ChangePercentage = -0.02957F;
+            var LastUpdated = DateTime.Now;
+
+            var holdings = new List<Holdings>
+            {
+                new Holdings
+                {
+                    Id = 2,
+                    AccountId = AccountId,
+                    CompanyName = "General Electric Co.",
+                    Symbol = "GE",
+                    StockCount = 5,
+                    LatestCostPerStock = LatestCostPerStock,
+                    Change = Change,
+                    ChangePercentage = ChangePercentage,
+                    LastUpdated = LastUpdated,
+                }
+            };
+
+            _session.Query<Holdings>().Returns(holdings.AsQueryable());
+
+            var resultHoldings = _sut.GetHoldings(AccountId);
+            Console.WriteLine(resultHoldings);
+            Assert.That(resultHoldings.Count, Is.EqualTo(1));
+
+            //_session.Received(1).SaveAsync(Arg.Is<Account>(a => a.Id == 0 && a.UserId == 3 && a.Balance == 100000));
+        }
+
+        
+    }
+}
