bb016c0f5efcb6a4314e1fca9cbead88f9659420
Eddie Cuellar <heriberto.cuellar1329@gmail.com>
2021-01-03 01:06:18 -0600

    working on Holding Service Tests

--- FantasyStockTradingApp/FantasyStockTradingApp.Tests/HoldingServiceTests.cs
+++ FantasyStockTradingApp/FantasyStockTradingApp.Tests/HoldingServiceTests.cs
@@ -57,12 +57,109 @@ namespace FantasyStockTradingApp.Tests
             _session.Query<Holdings>().Returns(holdings.AsQueryable());
 
             var resultHoldings = _sut.GetHoldings(AccountId);
-            Console.WriteLine(resultHoldings);
             Assert.That(resultHoldings.Count, Is.EqualTo(1));
-
-            //_session.Received(1).SaveAsync(Arg.Is<Account>(a => a.Id == 0 && a.UserId == 3 && a.Balance == 100000));
         }
 
-        
+        [Test]
+        public void GetHoldings_ShouldReturnTrue()
+        {
+            int AccountId = 2;
+            string Symbol = "GE";
+            float LatestCostPerStock = 10.65F;
+            float Change = -0.33F;
+            float ChangePercentage = -0.02957F;
+            var LastUpdated = DateTime.Now;
+
+            /*IList<Holdings> holdings = _session.QueryOver<Holdings>()
+                .Where(h => h.AccountId == AccountId)
+                .And(h => h.Symbol == Symbol)
+                .List();*/
+
+            var holdings = new List<Holdings>
+            {
+                new Holdings
+                {
+                    Id = 2,
+                    AccountId = AccountId,
+                    CompanyName = "General Electric Co.",
+                    Symbol = Symbol,
+                    StockCount = 5,
+                    LatestCostPerStock = LatestCostPerStock,
+                    Change = Change,
+                    ChangePercentage = ChangePercentage,
+                    LastUpdated = LastUpdated
+                }
+            };
+
+            //_session.QueryOver<Holdings>().Returns(holdings.ToList<Holdings>);
+            //_session.Query<Holdings>().Returns(holdings.AsQueryable());
+            _session.Query<Holdings>().Returns(holdings.AsQueryable());
+
+            bool resultHoldings = _sut.HoldingExists(AccountId, Symbol);
+            Assert.That(resultHoldings, Is.EqualTo(true));
+
+        }
+
+        [Test]
+        public void NewHolding_ShouldCreateHoldingAsync()
+        {
+            int AccountId = 2;
+            string CompanyName = "General Electric Co.";
+            string Symbol = "GE";
+            int StockCount = 5;
+            float LatestCostPerStock = 10.65F;
+            float Change = -0.33F; 
+            float ChangePercentage = -0.02957F;
+            var LastUpdated = DateTime.Now;
+
+            _sut.NewHolding(AccountId, CompanyName, Symbol, StockCount,
+                LatestCostPerStock, Change, ChangePercentage, LastUpdated);
+            _session.Received(1).SaveAsync(Arg.Any<Holdings>());
+
+        }
+
+        [Test]
+        public void UpdateHolding_ShouldUpdateHoldingAsync()
+        {
+            int AccountId = 2;
+            string CompanyName = "General Electric Co.";
+            string Symbol = "GE";
+            int StockCount = 5;
+            float LatestCostPerStock = 10.65F;
+            float Change = -0.33F;
+            float ChangePercentage = -0.02957F;
+            var LastUpdated = DateTime.Now;
+
+            _sut.UpdateHolding(AccountId, Symbol, StockCount, LatestCostPerStock, LastUpdated);
+
+            var query = _session.CreateQuery("Update Holdings set stock_count =:StockCount, " +
+                    "latest_cost_per_stock =:LatestCostPerStock, last_Updated =:LastUpdated " +
+                    "where account_id =:AccountId and symbol =:Symbol");
+
+            query.SetParameter("StockCount", StockCount);
+            query.SetParameter("LatestCostPerStock", LatestCostPerStock);
+            query.SetParameter("LastUpdated", LastUpdated);
+            query.SetParameter("AccountId", AccountId);
+            query.SetParameter("Symbol", Symbol);
+
+            var resultUpdate = query.Received(1).ExecuteUpdateAsync();
+            Assert.That(resultUpdate, Is.EqualTo(1));
+        }
+
+
+        /*
+          [Test]
+        public void NewAccount_ShouldCreateAccountAsync()
+        {
+            var UserId = 3;
+
+            _sut.NewAccount(UserId);
+            _session.Received(1).SaveAsync(Arg.Any<Account>());
+            //_session.Received(1).SaveAsync(Arg.Is<Account>(a => a.Id == 0 && a.UserId == 3 && a.Balance == 100000));
+
+
+        }
+         
+         */
     }
 }
