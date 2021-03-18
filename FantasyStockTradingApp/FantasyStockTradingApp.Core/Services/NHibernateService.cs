59f3461deb05b9bfbcf23b1cd6ba35df200195e1
Eddie Cuellar <heriberto.cuellar1329@gmail.com>
2021-03-17 23:16:11 -0500

    refactoring Nhibernate connection

--- FantasyStockTradingApp/FantasyStockTradingApp.Core/Services/NHibernateService.cs
+++ FantasyStockTradingApp/FantasyStockTradingApp.Core/Services/NHibernateService.cs
@@ -1,6 +1,7 @@
 ﻿using FluentNHibernate.Cfg;
 using FluentNHibernate.Cfg.Db;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using NHibernate;
 using NHibernate.Tool.hbm2ddl;
 using System;
@@ -17,10 +18,12 @@ namespace FantasyStockTradingApp.Core.Services
     {
         private readonly ISessionFactory _sessionFactory;
         private readonly IWebHostEnvironment _hostingEnvironment;
+        private readonly IConfiguration _configuration;
 
-        public NHibernateService(IWebHostEnvironment hostingEnvironment)
+        public NHibernateService(IWebHostEnvironment hostingEnvironment, IConfiguration configuration)
         {
             _hostingEnvironment = hostingEnvironment;
+            _configuration = configuration;
             _sessionFactory = FluentConfigure();
         }
         public ISession OpenSession()
@@ -40,36 +43,49 @@ namespace FantasyStockTradingApp.Core.Services
         }
         public ISessionFactory FluentConfigure()
         {
-            if (_hostingEnvironment.EnvironmentName == "Development")
-            {
-                return Fluently.Configure()
+            return Fluently.Configure()
                .Database(PostgreSQLConfiguration.PostgreSQL82
-               .ConnectionString(c =>
-                    c.Host("localhost")
-                    .Port(5432)
-                    .Database("FantasyStockTradingApp")
-                    .Username("postgres")
-                    .Password("postgres")))
+               .ConnectionString(c => c
+                .Host(_configuration["Database:Host"])
+                .Port(int.Parse(_configuration["Database:Port"]))
+                .Database(_configuration["Database:DbName"])
+                .Username(_configuration["Database:Username"])
+                .Password(_configuration["Database:Password"])))
                .Mappings(m => m.FluentMappings.AddFromAssembly(Assembly.GetExecutingAssembly()))
                .ExposeConfiguration(cfg => new SchemaUpdate(cfg).Execute(false, true))
                .BuildSessionFactory();
-            } else
-            {
-               var DBPASSWORD = Environment.GetEnvironmentVariable("DBPASSWORD");
-                return Fluently.Configure()
-               .Database(PostgreSQLConfiguration.PostgreSQL82
-               .ConnectionString(c =>
-                    c.Host("fantasystocktrader-hcuellar-postgresql.cukdhjozet53.us-east-2.rds.amazonaws.com")
-                    .Port(5432)
-                    .Database("postgres")
-                    .Username("postgreSQLAdmin")
-                    .Password(DBPASSWORD)
-                    )
-               )
-               .Mappings(m => m.FluentMappings.AddFromAssembly(Assembly.GetExecutingAssembly()))
-               .ExposeConfiguration(cfg => new SchemaUpdate(cfg).Execute(false, true))
-               .BuildSessionFactory();
-            }
+
+
+            // if (_hostingEnvironment.EnvironmentName == "Development")
+            // {
+            //     return Fluently.Configure()
+            //    .Database(PostgreSQLConfiguration.PostgreSQL82
+            //    .ConnectionString(c =>
+            //         c.Host("localhost")
+            //         .Port(5432)
+            //         .Database("FantasyStockTradingApp")
+            //         .Username("postgres")
+            //         .Password("postgres")))
+            //    .Mappings(m => m.FluentMappings.AddFromAssembly(Assembly.GetExecutingAssembly()))
+            //    .ExposeConfiguration(cfg => new SchemaUpdate(cfg).Execute(false, true))
+            //    .BuildSessionFactory();
+            // } else
+            // {
+            //    var DBPASSWORD = Environment.GetEnvironmentVariable("DBPASSWORD");
+            //     return Fluently.Configure()
+            //    .Database(PostgreSQLConfiguration.PostgreSQL82
+            //    .ConnectionString(c =>
+            //         c.Host("fantasystocktrader-hcuellar-postgresql.cukdhjozet53.us-east-2.rds.amazonaws.com")
+            //         .Port(5432)
+            //         .Database("postgres")
+            //         .Username("postgreSQLAdmin")
+            //         .Password(DBPASSWORD)
+            //         )
+            //    )
+            //    .Mappings(m => m.FluentMappings.AddFromAssembly(Assembly.GetExecutingAssembly()))
+            //    .ExposeConfiguration(cfg => new SchemaUpdate(cfg).Execute(false, true))
+            //    .BuildSessionFactory();
+            // }
         }
     }
 }
